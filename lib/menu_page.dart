d9b57774fe9e593cc859128fbdcce28b31fbd661
Jihyun Lee <jlee310@go.pasadena.edu>
2022-06-25 18:04:29 -0700

    Added draggable sheet

--- lib/menu_page.dart
+++ lib/menu_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:bottom_drawer/bottom_drawer.dart';
 import 'closet_page.dart';
 
 class MenuPage extends StatefulWidget {
@@ -11,36 +10,51 @@ class MenuPage extends StatefulWidget {
 
 class _MenuPageState extends State<MenuPage> {
 
-  BottomDrawerController controller = BottomDrawerController();
-
   final List<String> menu = <String> ['Outer', 'Top', 'Bottom', 'Dress', 'Shoes', 'Other'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white24,
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            flex: 1,
-            child: SizedBox(
-                height: 80,
-                width: 400,
-                child: Image.asset(
-                  'asset/bar.jpg',
-                  fit: BoxFit.cover,
+          Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: SizedBox(
+                    height: 80,
+                    width: 400,
+                    child: Image.asset(
+                      'asset/bar.jpg',
+                      fit: BoxFit.cover,
+                    ),
                 ),
-            ),
-          ),
-          Expanded(
-            flex: 8,
-            child: Stack(
+              ),
+              Expanded(
+                flex: 9,
+                child: Stack(
 
-            ),
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            flex: 1,
-            child: Stack(
+          bottomDetailsSheet(),
+        ]
+      ),
+    );
+  }
+
+  Widget bottomDetailsSheet() {
+    return DraggableScrollableSheet(
+      initialChildSize: 0.13,
+      minChildSize: 0.13,
+      maxChildSize: 0.8,
+      builder: (BuildContext context, ScrollController scrollController) {
+        return ListView(
+          controller: scrollController,
+          children: [
+            Stack(
               children: [
                 SizedBox(
                   height: 80,
@@ -50,33 +64,53 @@ class _MenuPageState extends State<MenuPage> {
                     fit: BoxFit.cover,
                   ),
                 ),
-                GestureDetector(
-                  onTap: () {
-                    controller.open();
-                    print("open drawer");
-                  }
+                Image.asset(''),
+              ]
+            ),
+
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => const ClosetPage()),
+                    // );
+                  },
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.only(left: 80, top: 33),
+                  ),
+                  child: Text (
+                    menu[0],
+                    style: const TextStyle (
+                      color: Colors.white,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => const ClosetPage()),
+                    // );
+                  },
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.only(left: 80, top: 33),
+                  ),
+                  child: Text (
+                    menu[1],
+                    style: const TextStyle (
+                      color: Colors.white,
+                      fontSize: 30,
+                    ),
+                  ),
                 ),
               ]
             ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget buildBottomDrawer(BuildContext context) {
-    return BottomDrawer(
-      header: Text("Here"),
-      /// your customized drawer body.
-      body: Text("Here"),
-      /// your customized drawer header height.
-      headerHeight: 60.0,
-      /// your customized drawer body height.
-      drawerHeight: 180.0,
-      /// drawer background color.
-      color: Colors.lightBlue,
-      /// drawer controller.
-      controller: controller,
+          ]
+        );
+      },
     );
   }
 }
